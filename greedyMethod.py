4e5e40f6239c1e623a53ccdbad68caff354c7039
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2020-11-17 16:00:28 -0600

    added dijkstra and greedyMethod

--- greedyMethod.py
+++ greedyMethod.py
@@ -16,10 +16,10 @@ def cronograma_optimo(trabajos, plazos, ganancias):
     space = list(range(1, max(plazos) + 1))
     optimo = []
     for i in range(len(trabajos)):
-        myList.append((trabajos[i], [plazos[i], ganancias[i]]))
+        myList.append([trabajos[i], [plazos[i], ganancias[i]]])
 
     sortedTable = sorted(myList, key=lambda x: x[1][1], reverse=True)
-    print(sortedTable)
+    print("Trabajos ordenados por ganancias (descendiente)", sortedTable)
 
     for i in range(len(sortedTable)):
         if len(space) != 0:
@@ -29,7 +29,36 @@ def cronograma_optimo(trabajos, plazos, ganancias):
             else:
                 pass
 
-    print(optimo)
+    print("El orden optimo de los trabajos es: ", optimo)
 
 
 cronograma_optimo(trabajos, plazos, ganancias)
+
+#  Ej 2: Programa que determine el patron optiom de combiancion de un conjunto de n listas
+identificador = ["a", "b", "c", "d", "e", "f"]
+datos = [40, 10, 20, 15, 25, 30]
+
+
+def combinacion_listas(identificador, datos):
+    myDict = {}
+    count = 0
+    operaciones = []
+    orden = []
+    for i in range(len(datos)):
+        myDict[identificador[i]] = datos[i]
+
+    sortedDict = {k: v for k, v in sorted(
+        myDict.items(), key=lambda item: item[1])}
+
+    for k, v in sortedDict.items():
+        count += v
+        operaciones.append(count)
+        orden.append(k)
+
+    operaciones.pop(0)
+    print("Secuendia de combinacion: ", orden,
+          "\n Cantidad de operaciones realizadas", operaciones, " = ", sum(operaciones))
+
+
+print()
+combinacion_listas(identificador, datos)
